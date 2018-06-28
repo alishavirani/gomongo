82c39c707cbac5cafcb0a7d0755d1cfcd2bac733
Amulya Kashyap <amulyakashyap09@gmail.com>
2018-06-28 15:34:06 +0000

    contants file added

---
+++ constants.go
@@ -0,0 +1,19 @@
+package MongoWrapper
+
+import (
+	"errors"
+)
+
+//Our Database types types
+const (
+	MYSQL   = "mysql"
+	SQLITE  = "sqlite"
+	MONGODB = "mongodb"
+)
+
+var (
+	MongoErrorNotFound = errors.New("not found") //especiall for mongo not found error | that's why "n" is in small letters | dont change it
+	ErrorNotFound      = errors.New("Data not found")
+	ErrorInvalidDBType = errors.New("Invalid database type")
+	ErrorInvalidDriver = errors.New("Unsupported storage driver!")
+)
