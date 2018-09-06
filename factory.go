17f043ad8713f489c2239271497f6de6d27d25a4
Amulya Kashyap <amulyakashyap09@gmail.com>
2018-09-06 21:15:59 +0530

    update the connect function

--- factory.go
+++ factory.go
@@ -21,37 +21,26 @@ func (n MongoDB) Connect(config *Config) (*Connection, error) {
 
 // ConnectMongo ...
 func ConnectMongo(config *Config) (*Connection, error) {
+
+	var mongoDBDialInfo *mgo.DialInfo
+	var mongoSession *mgo.Session
+	var err error
+
+	if len(config.Uri) > 0 {
+		mongoSession, err = mgo.Dial(config.Uri)
+	}else {
+		mongoDBDialInfo = &mgo.DialInfo{
+			Addrs:    []string{config.Hosts},
+			Timeout:  60 * time.Second,
+			Database: config.Database,
+			Source: config.AuthDatabase,
+			Username: config.Username,
+			Password: config.Password,
+		}
 	
+		mongoSession, err = mgo.DialWithInfo(mongoDBDialInfo)
+	}
 	
-	mongoDBDialInfo := &mgo.DialInfo{
-		Addrs:    []string{config.Hosts},
-		Timeout:  60 * time.Second,
-		Database: config.Database,
-		Source: config.AuthDatabase,
-		Username: config.Username,
-		Password: config.Password,
-	}
-
-	mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
-	if err != nil {
-		log.Println("error : ", err)
-		return nil, err
-	}
-
-	mongoSession.SetMode(mgo.Monotonic, true)
-
-	conn := new(Connection)
-	conn.Session = mongoSession
-	conn.Collections = make(map[string]*mgo.Collection)
-	conn.Session.DB(config.Database)
-	conn.Database = config.Database
-
-	return conn, nil
-}
-
-// Dial ...
-func Dial(config *Config) (*Connection, error) {
-	mongoSession, err := mgo.Dial(config.Uri)
 	if err != nil {
 		log.Println("error : ", err)
 		return nil, err
